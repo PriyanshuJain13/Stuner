ea50606b9419ea2b9709ffce7787220b180deee1
PriyanshuJain13 <85477522+PriyanshuJain13@users.noreply.github.com>
2022-11-19 14:30:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- preloader  -->
+    <div class="preloader">
+        <div class="preloader-icon">
+            <h2>HealthBliss</h2>
+            <h1>&#x2624;</h1>
+        </div>
+    </div>
+
+</body>
+
+</html>
