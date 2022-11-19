a48735d6c47155e07b4d947b0045e2b3cc46eaee
Ashish Chauhan <96202748+ashishchauhan0001@users.noreply.github.com>
2022-11-19 15:56:15 +0530

    Add files via upload

---
+++ project/register.php
@@ -0,0 +1,166 @@
+<?php
+require_once 'config.php';
+
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //if username is empty
+    if (empty(trim($_POST["username"]))) {
+        $username_err = "Username cannot be blank";
+    } else {
+        $sql = "SELECT id FROM user WHERE username = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+
+            //set the value of param username
+            $param_username = trim($_POST['username']);
+
+            //try to execute statement
+            if (mysqli_stmt_execute($stmt)) {
+                mysqli_stmt_store_result($stmt);
+                if (mysqli_stmt_num_rows($stmt) == 1) {
+                    $username_err = "This username is already taken";
+                } else {
+                    $username = trim($_POST['username']);
+                }
+            } else {
+                echo "Something went wrong";
+            }
+        }
+    }
+
+    mysqli_stmt_close($stmt);
+
+
+    //check for password
+
+    if (empty(trim($_POST['password']))) {
+        $password_err = "Password cannot be blank";
+    } else if (strlen(trim($_POST['password'])) < 5) {
+        $password_err = "Password too short";
+    } else {
+        $password = trim($_POST['password']);
+    }
+
+    //check for confirm password field
+    if (empty(trim($_POST['confirm_password']))) {
+        $confirm_password_err = "This field cannot be empty";
+    } else if (trim($_POST['confirm_password']) != trim($_POST['password'])) {
+        $confirm_password_err = "Password doesnnot match";
+    }
+
+    //INSERT IN DATABASE
+    if (empty($username_err) && empty($confirm_password_err) && empty($password_err)) {
+        $sql = "INSERT INTO user (username,password) VALUES (?,?)";
+        $stmt = mysqli_prepare($conn, $sql);
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+
+            //SET PARAMS
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+
+            //EXECUTE QUERY
+            if (mysqli_stmt_execute($stmt)) {
+                header("location: login.php");
+            } else {
+                echo "Something went wrong... cannot redirect!";
+            }
+        }
+
+        mysqli_stmt_close($stmt);
+    }
+
+    mysqli_close($conn);
+}
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>PHP login system!</title>
+</head>
+
+<body>
+<header id="header">
+        <a href="index.php"> <img id="logo" src="images/jiitlogo-removebg-preview.png" alt=""></a>
+        <nav class="navbar">
+            <ul style="padding-top: 2rem;" class="nav-ul">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Market data</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="register.php">Register</a></li>
+            </ul>
+        </nav>
+        <div id="dropdown">
+            <button><i class="fa-regular fa-user"></i><i class="fa-solid fa-angle-down"></i></button> 
+            <div class="drop-content">
+                <a href="profile.php">Profile</a>
+                <a href="logout.php">sign-out</a>    
+            </div>
+        </div>
+    </header>
+    <div class="container mt-4">
+        <h3>Please Register Here:</h3>
+        <hr>
+        <form action="" method="post">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputEmail4">Username</label>
+                    <input type="text" class="form-control" name="username" id="inputEmail4" placeholder="Username">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputPassword4">Password</label>
+                    <input type="password" class="form-control" name="password" id="inputPassword4" placeholder="Password">
+                </div>
+            </div>
+            <div class="form-group">
+                <label for="inputPassword4">Confirm Password</label>
+                <input type="password" class="form-control" name="confirm_password" id="inputPassword" placeholder="Confirm Password">
+            </div>
+            <div class="form-group">
+                <label for="inputAddress2">Address </label>
+                <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+            </div>
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputState">State</label>
+                    <select id="inputState" class="form-control">
+                        <option selected>Choose...</option>
+                        <option>...</option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Zip</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary ">Sign up</button>
+        </form>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
